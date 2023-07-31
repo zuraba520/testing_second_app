bbe39f574b2aff44c8b2156158e29bab616a88e4
gizu <zuraba000@gmail.com>
2023-07-31 13:14:21 +0400

    feat(add new page,answers and questions)

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:testing_second_app/answer_button.dart';
+import 'package:testing_second_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
- const  QuestionsScreen({super.key});
+  const QuestionsScreen({super.key});
+
   @override
   State<QuestionsScreen> createState() {
- return _QuiestionsScreenState();
+    return _QuiestionsScreenState();
   }
 }
-
 
 class _QuiestionsScreenState extends State<QuestionsScreen> {
   @override
-  Widget build( context) {
-    return const Text('QuestionsScreen');
+  Widget build(context) {
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity, // rom gaiwelos mtlian ekranze fyvelaferi
+      child: Column(
+        // morgebuli iyos es pasuxebi centrshi
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white70,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: currentQuestion.answers[0],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[1],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[2],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[3],
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
-
-
-
